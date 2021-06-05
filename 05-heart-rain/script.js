04d98f94264688b4deb5e25818a8c7ab84bcf08a
gsmperera <gsmpereraw@gmail.com>
2021-06-06 04:38:25 +0530

    updated

--- 05-heart-rain/script.js
+++ 05-heart-rain/script.js
@@ -1,11 +1,11 @@
 function createHeart() {
-  const heart = document.createElement("div");
-  heart.classList.add("heart");
-  heart.innerText = "🤍";
-  heart.style.left = Math.random() * 100 + "vw";
-  heart.style.animationDuration = Math.random() * 3 + 2 + "s";
-  heart.style.fontSize = Math.random() * 10 + 10 + "px";
-  document.body.appendChild(heart);
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.innerText = '🤍';
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 3 + 2 + 's';
+    heart.style.fontSize = Math.random() * 10 + 10 + 'px';
+    document.body.appendChild(heart);
 }
 
 setInterval(createHeart, 300);
